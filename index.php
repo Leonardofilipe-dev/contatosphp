19b7b65cc720f19e8efdb965ca0abb4d937aa7ce
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-06-23 17:48:29 -0300

    Criando conexão com banco de dados petodo post

--- index.php
+++ index.php
@@ -1,40 +1,23 @@
-<html lang="pt-br">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <title>Agenda de Contatos</title>
+    <title>Formulário de Contato</title>
 </head>
 <body>
-   <h1>Agenda de contatos</h1>
-
-   
-    <form action="./create.php" method="post">
-        <label for="name">Contact: </label><br>
-        <input type="text" id="name" name="username"><br>
-
-        <label for="email">Email: </label><br>
-        <input type="email" id="email" name="useremail"><br>
-
-        <label for="phone">Telefone</label><br>
-        <input type="number" id="phone" name="phonenumber"><br>
-        
-        <input type="submit" id="btn" value="Enviar">
-
-
-
+    <h1>Formulário de Contato</h1>
+    <form method="post" action="./create.php">
+        <label for="username">Nome:</label>
+        <input type="text" name="username" id="username" required>
+        <br><br>
+        <label for="useremail">Email:</label>
+        <input type="email" name="useremail" id="useremail" required>
+        <br><br>
+        <label for="phonenumber">Telefone:</label>
+        <input type="text" name="phonenumber" id="phonenumber" required>
+        <br><br>
+        <input type="submit" value="Enviar">
     </form>
-
-        
-        <h2>Lista de Contatos:</h2>
-    <?php
-    
-    $listaContatos = file_get_contents('list.php');
-    $contatos = explode("\n", $listaContatos);
-    foreach ($contatos as $contato) {
-        echo $contato . "<br>";
-    }
-    ?>
 </body>
 </html>
+
 
